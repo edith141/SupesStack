b2fa1cc85e88d313a8d20ff73c6591fe4eec05d8
Ishaan <53191150+edith141@users.noreply.github.com>
2020-02-05 15:51:57 +0530

    added routes, fixed views, etc

--- sqrd.js
+++ sqrd.js
@@ -1,64 +1,82 @@
-var app = angular.module('sqrdApp', []);
-            app.controller('sqrdCtrl', ['$scope', function($scope) {
+var app = angular.module('sqrdApp', ['ngRoute']);
 
-                $scope.removeSupe = function(supe) {
-                    // console.log(supe)
-                    var removedSupe = $scope.supes.indexOf(supe);
-                    // console.log(removedSupe);
-                    $scope.supes.splice(removedSupe, 1);
-                }
+app.config(['$routeProvider', function($routeProvider) {
 
-                $scope.addSupe = function() {
-                    $scope.supes.push({
-                        name: $scope.sqrdCtrl.newsupe.name,
-                        belt: $scope.sqrdCtrl.newsupe.belt,
-                        rate: parseInt ($scope.sqrdCtrl.newsupe.rate),
-                        available: true,
-                        thumb: "https://avatar.oxro.io/avatar?name=" + $scope.sqrdCtrl.newsupe.name
-                    })
+    $routeProvider.when('/home', {
+        templateUrl: "views/home.html"
+    }).when('/directory', {
+        templateUrl: "views/directory.html",
+        controller: 'sqrdCtrl'
+    }).otherwise({
+        redirectTo: '/home'
+    })
+}]);
 
-                    $scope.sqrdCtrl.newsupe.name =  $scope.sqrdCtrl.newsupe.belt = $scope.sqrdCtrl.newsupe.rate = ""
-                }
+app.controller('sqrdCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.removeSupe = function(supe) {
+        // console.log(supe)
+        var removedSupe = $scope.supes.indexOf(supe);
+        // console.log(removedSupe);
+        $scope.supes.splice(removedSupe, 1);
+    }
+
+    $scope.addSupe = function() {
+        $scope.supes.push({
+            name: $scope.sqrdCtrl.newsupe.name,
+            belt: $scope.sqrdCtrl.newsupe.belt,
+            rate: parseInt ($scope.sqrdCtrl.newsupe.rate),
+            available: true,
+            thumb: "https://avatar.oxro.io/avatar?name=" + $scope.sqrdCtrl.newsupe.name
+        })
+
+        $scope.sqrdCtrl.newsupe.name =  $scope.sqrdCtrl.newsupe.belt = $scope.sqrdCtrl.newsupe.rate = ""
+    };
+
+    // $http.get('data/heroes.json').then(function(data) {
+    //     $scope.supes = data;
+    // }, function(err) {console.log(err)});
 
 
 
-                $scope.fname ='Edith';
-                // $scope.fnum =  ;
-                $scope.squareNum = $scope.fnum * $scope.fnum;
-                $scope.supes = [
-                    {name: 'batman',
-                    belt: 'green',
-                    rate: 90,
-                    available: true,
-                    thumb: "https://avatar.oxro.io/avatar?name=" + 'bat man'}, 
-                    {name: 'ironman',
-                    belt: 'red',
-                    rate: 90,
-                    available: true,
-                    thumb: "https://avatar.oxro.io/avatar?name=" + 'Iron man'}, 
-                    {name: 'spiderman',
-                    belt: 'blue',
-                    rate: 850,
-                    available: true,
-                    thumb: "https://avatar.oxro.io/avatar?name=" + 'Spider Man'}, 
-                    {name: 'wonderwoman',
-                    belt: 'black',
-                    rate: 60,
-                    available: false,
-                    thumb: "https://avatar.oxro.io/avatar?name=" + 'Wonder Woman'}];
+    $scope.fname ='Edith';
+    // $scope.fnum =  ;
+    $scope.squareNum = $scope.fnum * $scope.fnum;
+    $scope.supes = [
+        {name: 'batman',
+        belt: 'green',
+        rate: 90,
+        available: true,
+        thumb: "https://avatar.oxro.io/avatar?name=" + 'bat man'}, 
+        {name: 'ironman',
+        belt: 'red',
+        rate: 90,
+        available: true,
+        thumb: "https://avatar.oxro.io/avatar?name=" + 'Iron man'}, 
+        {name: 'spiderman',
+        belt: 'blue',
+        rate: 850,
+        available: true,
+        thumb: "https://avatar.oxro.io/avatar?name=" + 'Spider Man'}, 
+        {name: 'wonderwoman',
+        belt: 'black',
+        rate: 60,
+        available: false,
+        thumb: "https://avatar.oxro.io/avatar?name=" + 'Wonder Woman'}];
 
-                // console.log($scope.squareNum);
-                console.log($scope.fnum);
-                console.log($scope.fname);
+    // console.log($scope.squareNum);
+    console.log($scope.fnum);
+    console.log(angular.toJson($scope.supes));
+    console.log($scope.fname);
 
-                $scope.sqrFun = function () {
-                    $scope.squareNum = $scope.fnum * $scope.fnum;
-                };
-                // let nums = [1,2,3,4,5];
-            }]);
+    $scope.sqrFun = function () {
+        $scope.squareNum = $scope.fnum * $scope.fnum;
+    };
+    // let nums = [1,2,3,4,5];
+}]);
 
-            // app.directive("w3TestDirective", function() {
-            //     return {
-            //       num : "<h1>Made by a directive!</h1>"
-            //     };
-            // });
+// app.directive("w3TestDirective", function() {
+//     return {
+//       num : "<h1>Made by a directive!</h1>"
+//     };
+// });
